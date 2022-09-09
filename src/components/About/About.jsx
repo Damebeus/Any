79513c449dd0f634560dd42e65fc295006ddcc57
Damebeus <99093997+Damebeus@users.noreply.github.com>
2022-09-09 19:23:14 -0300

    about listo

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 import style from "./About.module.css";
+import logo from "../../assets/blacklogo.png";
 const About = () => {
-  return <div className={style.container}>About</div>;
+  return (
+    <section className={style.section} id='About'>
+      <div className={style.container}>
+        <div className={style.info}>
+          <div className={style.subtitulo}>
+            <h2>we offer the confidence you need</h2>
+          </div>
+          <div className={style.titulo}>
+            <h1>Any cpu technology</h1>
+          </div>
+          <div className={style.lorem}>
+            lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+            <span>Lorem ipsum </span>
+            dolor sit amet consectetur adipisicing elit. Vel iste expedita magni
+            quod, id dolorem, et doloribus, praesentium excepturi laboriosam
+            consequatur quibusdam natus cupiditate repellat molestiae
+            dignissimos sapiente impedit earum.
+          </div>
+          <div className={style.boton}>
+            <button>
+              <span>Contact me</span>
+            </button>
+          </div>
+        </div>
+        <div className={style.foto}>
+          <img src={logo} />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,58 @@
 import React from "react";
 import { Link } from "react-scroll";
 import style from "./Navbar.module.css";
+import logo from "../../assets/horizontalblack.png";
 
 const Navbar = () => {
-  return <div className={style.botones}></div>;
+  return (
+    <div className={style.container}>
+      <div className={style.logo}>
+        <img src={logo} />
+      </div>
+      <div className={style.botones}>
+        <Link
+          activeClass={style.selectedLink}
+          className={style.links}
+          to='Home'
+          spy={true}
+          smooth={true}
+          duration={800}
+        >
+          Home
+        </Link>
+        <Link
+          activeClass={style.selectedLink}
+          className={style.links}
+          to='About'
+          spy={true}
+          smooth={true}
+          duration={800}
+        >
+          About
+        </Link>
+        <Link
+          activeClass={style.selectedLink}
+          className={style.links}
+          to='Projects'
+          spy={true}
+          smooth={true}
+          duration={800}
+        >
+          Projects
+        </Link>
+        <Link
+          activeClass={style.selectedLink}
+          className={style.links}
+          to='Contact'
+          spy={true}
+          smooth={true}
+          duration={800}
+        >
+          Contact
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
