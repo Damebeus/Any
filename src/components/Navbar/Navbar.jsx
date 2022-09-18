3d79187da35e6ff9ebbb53a59964fd324b9df7e8
Damebeus <99093997+Damebeus@users.noreply.github.com>
2022-09-18 18:19:49 -0300

    mobile welcome

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -73,6 +73,75 @@ const Navbarr = () => {
           {t("NavBar.Contact")}
         </Link>
       </div>
+      <div className={style.mobile}></div>
+      <nav class='navbar navbar-expand-lg bg-success  bg-success position-fixed top-0  w-100 '>
+        <div class='container-fluid '>
+          <a class='navbar-brand' href='#'>
+            Menu
+          </a>
+          <button
+            class='navbar-toggler'
+            type='button'
+            data-bs-toggle='collapse'
+            data-bs-target='#navbarNav'
+            aria-controls='navbarNav'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+          >
+            <span class='navbar-toggler-icon'></span>
+          </button>
+          <div class='collapse navbar-collapse ' id='navbarNav'>
+            <ul class='navbar-nav '>
+              <button class='nav-item' className={style.boton}>
+                <Link to='promociones'>
+                  <a class='nav-link active' href='#'>
+                    Promociones
+                  </a>
+                </Link>
+              </button>
+              <button class='nav-item' className={style.boton}>
+                <Link to='empanadas'>
+                  <a class='nav-link active' href='#'>
+                    Empanadas
+                  </a>
+                </Link>
+              </button>
+              <button class='nav-item' className={style.boton}>
+                <Link to='pizza'>
+                  <a class='nav-link active' href='#'>
+                    Pizzas
+                  </a>
+                </Link>
+              </button>
+              <button class='nav-item' className={style.boton}>
+                <Link to='sanguches'>
+                  <a class='nav-link active'>Milanesas</a>
+                </Link>
+              </button>
+              <button class='nav-item' className={style.boton}>
+                <Link to='lomitos'>
+                  <a class='nav-link active'>Lomos</a>
+                </Link>
+              </button>
+              <button class='nav-item' className={style.boton}>
+                <Link to='burguer'>
+                  <a class='nav-link active'>Hamburguesas</a>
+                </Link>
+              </button>
+              <button class='nav-item' className={style.boton}>
+                <Link to='Bebidas'>
+                  <a class='nav-link active'>Bebidas</a>
+                </Link>
+              </button>
+              <button class='nav-item' className={style.boton}>
+                <a class='nav-link active' aria-current='page' href='/cart'>
+                  Carrito
+                </a>
+              </button>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
