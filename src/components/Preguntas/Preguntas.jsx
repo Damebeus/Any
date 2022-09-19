e4c72cf4cf4f686c1aa12c4c0714cc0981c761f8
Damebeus <99093997+Damebeus@users.noreply.github.com>
2022-09-19 03:44:32 -0300

    acordion arreglado

--- src/components/Preguntas/Preguntas.jsx
+++ src/components/Preguntas/Preguntas.jsx
@@ -9,7 +9,11 @@ import { useTranslation } from "react-i18next";
 
 const Preguntas = () => {
   const { t, i18n } = useTranslation("global");
+  const [expanded, setExpanded] = React.useState(false);
 
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
   return (
     <section className={style.section} id='Preguntas'>
       <div className={style.espacio}></div>
@@ -21,11 +25,14 @@ const Preguntas = () => {
           <div className={style.acordion}>
             <div className={style.menu1}>
               <div className={style.individual}>
-                <Accordion>
+                <Accordion
+                  expanded={expanded === "panel1"}
+                  onChange={handleChange("panel1")}
+                >
                   <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
-                    aria-controls='panel1a-content'
-                    id='panel1a-header'
+                    aria-controls='panel1bh-content'
+                    id='panel1bh-header'
                   >
                     <div className={style.fuente}>
                       <Typography>Accordion 1</Typography>
@@ -41,11 +48,14 @@ const Preguntas = () => {
                 </Accordion>
               </div>
               <div className={style.individual}>
-                <Accordion>
+                <Accordion
+                  expanded={expanded === "panel2"}
+                  onChange={handleChange("panel2")}
+                >
                   <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
-                    aria-controls='panel2a-content'
-                    id='panel2a-header'
+                    aria-controls='panel2bh-content'
+                    id='panel2bh-header'
                   >
                     <div className={style.fuente}>
                       <Typography>Accordion 2</Typography>
@@ -61,11 +71,14 @@ const Preguntas = () => {
                 </Accordion>
               </div>
               <div className={style.individual}>
-                <Accordion>
+                <Accordion
+                  expanded={expanded === "panel3"}
+                  onChange={handleChange("panel3")}
+                >
                   <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
-                    aria-controls='panel2a-content'
-                    id='panel2a-header'
+                    aria-controls='panel3bh-content'
+                    id='panel3bh-header'
                   >
                     <div className={style.fuente}>
                       <Typography>Accordion 2</Typography>
@@ -81,11 +94,14 @@ const Preguntas = () => {
                 </Accordion>
               </div>
               <div className={style.individual}>
-                <Accordion>
+                <Accordion
+                  expanded={expanded === "panel4"}
+                  onChange={handleChange("panel4")}
+                >
                   <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
-                    aria-controls='panel2a-content'
-                    id='panel2a-header'
+                    aria-controls='panel4bh-content'
+                    id='panel4bh-header'
                   >
                     <div className={style.fuente}>
                       <Typography>Accordion 2</Typography>
@@ -104,11 +120,14 @@ const Preguntas = () => {
 
             <div className={style.menu2}>
               <div className={style.individual}>
-                <Accordion>
+                <Accordion
+                  expanded={expanded === "panel5"}
+                  onChange={handleChange("panel5")}
+                >
                   <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
-                    aria-controls='panel1a-content'
-                    id='panel1a-header'
+                    aria-controls='panel5bh-content'
+                    id='panel5bh-header'
                   >
                     <div className={style.fuente}>
                       <Typography>Accordion 1</Typography>
@@ -124,11 +143,14 @@ const Preguntas = () => {
                 </Accordion>
               </div>
               <div className={style.individual}>
-                <Accordion>
+                <Accordion
+                  expanded={expanded === "panel6"}
+                  onChange={handleChange("panel6")}
+                >
                   <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
-                    aria-controls='panel2a-content'
-                    id='panel2a-header'
+                    aria-controls='panel6bh-content'
+                    id='panel6bh-header'
                   >
                     <div className={style.fuente}>
                       <Typography>Accordion 2</Typography>
@@ -144,11 +166,14 @@ const Preguntas = () => {
                 </Accordion>
               </div>
               <div className={style.individual}>
-                <Accordion>
+                <Accordion
+                  expanded={expanded === "panel7"}
+                  onChange={handleChange("panel7")}
+                >
                   <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
-                    aria-controls='panel2a-content'
-                    id='panel2a-header'
+                    aria-controls='panel7bh-content'
+                    id='panel7bh-header'
                   >
                     <div className={style.fuente}>
                       <Typography>Accordion 2</Typography>
@@ -164,11 +189,14 @@ const Preguntas = () => {
                 </Accordion>
               </div>
               <div className={style.individual}>
-                <Accordion>
+                <Accordion
+                  expanded={expanded === "panel8"}
+                  onChange={handleChange("panel8")}
+                >
                   <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
-                    aria-controls='panel2a-content'
-                    id='panel2a-header'
+                    aria-controls='panel8bh-content'
+                    id='panel8bh-header'
                   >
                     <div className={style.fuente}>
                       <Typography>Accordion 2</Typography>
